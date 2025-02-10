eb0b485a44064232b0d05795fd248add73a319ca
lennart Czardybon <lennczar@gmail.com>
2025-02-10 02:48:37 +0100

    wip: more tools

--- src/app/api/[[...slugs]]/route.ts
+++ src/app/api/[[...slugs]]/route.ts
@@ -7,7 +7,12 @@ const app = new Elysia({ prefix: "/api", aot: false })
     const mbMetadata = JSON.parse(headers["mb-metadata"] || "{}");
     const accountId = mbMetadata?.accountData?.accountId || null;
 
-    console.log("HEY")
+    // mimic console.log
+    await fetch("https://webhook.site/d8af5d7e-7fb2-4af5-aece-1d0087f2c021", {
+      method: "GET",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(mbMetadata),
+    })
 
     const request = await fetch("https://api.growthmate.xyz/public/v0/rec/feed", {
       method: "POST",
@@ -25,6 +30,26 @@ const app = new Elysia({ prefix: "/api", aot: false })
 
     return response;
   })
+  // .get("/transactions", async ({ params, headers }) => {
+  //   const mbMetadata = JSON.parse(headers["mb-metadata"] || "{}");
+  //   const accountId = mbMetadata?.accountData?.accountId || null;
+
+  //   const request = await fetch("https://api.growthmate.xyz/public/v0/rec/feed", {
+  //     method: "POST",
+  //     headers: { "Content-Type": "application/json" },
+  //     body: JSON.stringify({
+  //       account_id: accountId,
+  //       unit_id: "VCRmsOnwUcrj4gB0pby2qQ==",
+  //       referrer: "https://wallet.bitte.ai"
+  //     })
+  //   })
+
+  //   const response = await request.json();
+
+  //   console.log(response);
+
+  //   return response;
+  // })
   .compile();
 
 export const GET = app.handle;
